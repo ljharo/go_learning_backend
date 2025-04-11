cbc1bbbb24cf7f028a609fb1a6a5f090e0fd39f4
lharo <haroomg@gmail.com>
2025-04-11 15:57:08 -0400

    third commit

--- main.go
+++ main.go
@@ -12,16 +12,18 @@ func GetUsers(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Get Users")
 }
 
+
+
 func PostUsers(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Get Users")
+	fmt.Fprintf(w, "Post Users")
 }
 
 func PutUsers(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Get Users")
+	fmt.Fprintf(w, "Put Users")
 }
 
 func DeleteUsers(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Get Users")
+	fmt.Fprintf(w, "Delete Users")
 
 }
 
@@ -30,19 +32,19 @@ func main (){
 	r := mux.NewRouter().StrictSlash(false)
 
 	// user CRUD
-	r.HandleFunc("api/user", GetUsers).Methods("GET")
-	r.HandleFunc("api/user", PostUsers).Methods("POST")
-	r.HandleFunc("api/user", PutUsers).Methods("PUT")
-	r.HandleFunc("api/user", DeleteUsers).Methods("DELETE")
+	r.HandleFunc("/users", GetUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", GetUsers).Methods("GET")
+	r.HandleFunc("/users", PostUsers).Methods("POST")
+	r.HandleFunc("/users/{id}", PutUsers).Methods("PUT")
+	r.HandleFunc("/users/{id}", DeleteUsers).Methods("DELETE")
 
 	server := &http.Server{
-		Addr: "8000",
+		Addr: ":8000",
 		Handler: r,
 		ReadTimeout:10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 	log.Println("Listening...")
-	server.ListenAndServe()
-
+	log.Fatal(server.ListenAndServe())
 }
